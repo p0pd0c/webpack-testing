33647c52b5ff6e90783fe09351e54fac17f751f2
Saugus Catholics <sauguscatholics@Sauguss-MacBook-Pro.local>
2021-06-13 18:45:58 -0400

    Add react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import _ from 'lodash'
 import './style.sass'
-import Icon from './watermelon.jpeg'
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash imported by script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-
-    const watermelon = new Image()
-    watermelon.src = Icon
-    element.appendChild(watermelon)
-    return element;
-}
-  
-document.body.appendChild(component());
+import React from 'react'
+import {render} from 'react-dom'
+import App from './App'
+render(document.querySelector('root'), <App />)
